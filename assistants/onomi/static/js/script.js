21264285b5b54f3f4df4b3cd38da838ab6855543
Andrest2199 <128836304+Andrest2199@users.noreply.github.com>
2024-11-26 12:20:32 -0600

    Script.js change

--- assistants/onomi/static/js/script.js
+++ assistants/onomi/static/js/script.js
@@ -42,10 +42,11 @@ async function fetchAssistantResponse(question,thread_id="") {
     },
     body: JSON.stringify({
       question: question,
-      id_employee: "095393",
+      id_employee: "2004",
       compania: "1",
-      database: "FROM FRONT 123",
+      database: "FROM FRONT 1",
       thread_id: thread_id,
+      is_admin: false
     }),
   });
 
